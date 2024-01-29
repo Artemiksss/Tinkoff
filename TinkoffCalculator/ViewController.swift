2e050fb486a581b9c1ad08e74d8ce40e73d48bd3
Artemiksss <artem.zhovnir@yandex.ru>
2024-01-29 16:59:04 +0300

    add new fanc

--- TinkoffCalculator/ViewController.swift
+++ TinkoffCalculator/ViewController.swift
@@ -10,12 +10,17 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBAction func buttonPressed(_ sender: UIButton) {
-        guard let buttonText = sender.currentTitle else { return }
-        print(buttonText)
+        if let buttonTitle = sender.titleLabel?.text {
+            print(buttonTitle)
+        }
     }
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    @IBAction func test(_ sender: UIButton) {
+        guard let buttonText = sender.currentTitle else { return }
+        print(buttonText)
     }
 }
 
